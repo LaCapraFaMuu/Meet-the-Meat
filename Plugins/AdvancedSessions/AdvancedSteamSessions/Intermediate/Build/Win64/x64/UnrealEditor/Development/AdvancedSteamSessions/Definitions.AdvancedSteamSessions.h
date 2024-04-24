cc7511b3fc11b43e756269b0607f4f3604f9cffa
PippoPotamo <fscolari@cerebotani.it>
2024-04-24 22:32:02 +0200

    add weapon and animations for it

--- Plugins/AdvancedSessions/AdvancedSteamSessions/Intermediate/Build/Win64/x64/UnrealEditor/Development/AdvancedSteamSessions/Definitions.AdvancedSteamSessions.h
+++ Plugins/AdvancedSessions/AdvancedSteamSessions/Intermediate/Build/Win64/x64/UnrealEditor/Development/AdvancedSteamSessions/Definitions.AdvancedSteamSessions.h
@@ -1,4 +1,4 @@
-#include "C:/Users/codic/OneDrive/Desktop/prova/Game/Intermediate/Build/Win64/x64/GameEditor/Development/Engine/SharedDefinitions.Engine.Cpp20.InclOrderUnreal5_0.h"
+#include "C:/ProgettiUE5/Game/Intermediate/Build/Win64/x64/GameEditor/Development/Engine/SharedDefinitions.Engine.Cpp20.InclOrderUnreal5_0.h"
 #undef UE_IS_ENGINE_MODULE
 #undef DEPRECATED_FORGAME
 #define DEPRECATED_FORGAME DEPRECATED
