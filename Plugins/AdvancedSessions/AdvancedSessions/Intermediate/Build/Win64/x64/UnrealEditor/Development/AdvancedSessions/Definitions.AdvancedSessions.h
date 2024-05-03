6a2bc84479a1a6c0089e5bd6b5830b55596f4d02
LaCapraFaMuu <lnodari@cerebotani.it>
2024-05-03 19:47:58 +0200

    Fixato rotolamento veicoli + peso player (Filippo lo aveva fatto obeso come lui)

--- Plugins/AdvancedSessions/AdvancedSessions/Intermediate/Build/Win64/x64/UnrealEditor/Development/AdvancedSessions/Definitions.AdvancedSessions.h
+++ Plugins/AdvancedSessions/AdvancedSessions/Intermediate/Build/Win64/x64/UnrealEditor/Development/AdvancedSessions/Definitions.AdvancedSessions.h
@@ -1,4 +1,4 @@
-#include "C:/ProgettiUE5/Game/Intermediate/Build/Win64/x64/GameEditor/Development/Engine/SharedDefinitions.Engine.Cpp20.InclOrderUnreal5_0.h"
+#include "D:/Leo/Coding/Unreal Enigne/Game/Intermediate/Build/Win64/x64/GameEditor/Development/Engine/SharedDefinitions.Engine.Cpp20.InclOrderUnreal5_0.h"
 #undef UE_IS_ENGINE_MODULE
 #undef DEPRECATED_FORGAME
 #define DEPRECATED_FORGAME DEPRECATED
